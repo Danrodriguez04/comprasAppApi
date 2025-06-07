0af8a7b459f1c4b136d7ca6ca6da44f29a55ab84
Daniel <danielrodriguez@carnicassunyer.com>
2025-06-07 08:46:27 +0200

    init api repository

---
+++ src/main/java/com/comprasapp/comprasapp/ComprasAppApplication.java
@@ -0,0 +1,13 @@
+package com.comprasapp.comprasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprasAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComprasAppApplication.class, args);
+    }
+
+}
